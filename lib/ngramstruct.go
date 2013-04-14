f08d5ef71c5446d6fe4403495ff7007b3b1f5ba8
Michael Hughes <michael@mihughes.com>
2013-04-14 14:26:49 -0700

    add comments

--- lib/ngramstruct.go
+++ lib/ngramstruct.go
@@ -6,12 +6,16 @@ import (
     'strings'
 )
 
+//Ngram constains a single ngram processed for a text corpus
+//The structure stores whether the ngram began sentence and
+//or ended a sentence in addition to the actual parsed text
 type Ngram struct {
     Values []string
     IsStop boolean
     IsBeginner boolean
 }
 
+//Ngrams a containing data structure intended to provide fast access to many ngrams
 type Ngrams struct {
     ngrams map[string] *list.List
 }
@@ -21,10 +25,12 @@ type NgramReader interface {
     GetNextGram(string) (Ngram,boolean)
 }
 
+//NgramBuilder defines the Consume method signature
 type NgramBuilder interface {
-    Consume(reader *scanner.Scanner, n int) int
+    Consume(ngrams *Ngrams) (reader *scanner.Scanner, n int) int
 }
 
+//InitMemory initializes an in-memory ngram storage object
 function InitMemory() *Ngrams {
     ngrams := new(Ngrams)
     ngrams.ngrams = make(map[string]list.List)
